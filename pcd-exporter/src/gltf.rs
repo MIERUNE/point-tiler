e58ed4a71e08455fb70196851c7cb09c4522474c
nokonoko1203 <healtheworld.1203@gmail.com>
2024-11-06 18:13:59 +0900

    f

--- pcd-exporter/src/gltf.rs
+++ pcd-exporter/src/gltf.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, error::Error, io::Write};
 
+use byteorder::{ByteOrder as _, LittleEndian};
 use cesiumtiles_gltf_json::{
     Accessor, AccessorType, Buffer, BufferView, BufferViewTarget, ComponentType, Gltf, Mesh,
     MeshPrimitive, Node, Scene,
@@ -14,37 +15,43 @@ pub fn write_glb<W: Write>(
     let mut gltf_buffer_views = Vec::new();
     let mut gltf_accessors = Vec::new();
 
-    const VERTEX_BYTE_STRIDE: usize = 4 * 3; // 4byte(i32) x 3 (x, y, z)
-    const COLOR_BYTE_STRIDE: usize = 2 * 4; // 2byte(u16) x 3 (r, g, b)
+    // const VERTEX_BYTE_STRIDE: usize = 4 * 6; // 4byte(i32) x 3 (x, y, z)
+    // const COLOR_BYTE_STRIDE: usize = 2 * 4; // 2byte(u16) x 3 (r, g, b)
+    const BYTE_STRIDE: usize = 8 * 6;
 
     let buffer_offset = bin_content.len();
-    let mut buffer = [0u8; (VERTEX_BYTE_STRIDE + COLOR_BYTE_STRIDE)];
+    let mut buffer = [0u8; BYTE_STRIDE];
+
+    let mut position_max = [f64::MIN; 3];
+    let mut position_min = [f64::MAX; 3];
 
     for point in &points.points {
-        let x = point.x.to_le_bytes();
-        let y = point.y.to_le_bytes();
-        let z = point.z.to_le_bytes();
-        let r = point.color.r.to_le_bytes();
-        let g = point.color.g.to_le_bytes();
-        let b = point.color.b.to_le_bytes();
+        let x = point.x as f64;
+        let y = point.y as f64;
+        let z = point.z as f64;
+        let r = point.color.r as f64;
+        let g = point.color.g as f64;
+        let b = point.color.b as f64;
 
-        buffer[0..4].copy_from_slice(&x);
-        buffer[4..8].copy_from_slice(&y);
-        buffer[8..12].copy_from_slice(&z);
-        buffer[12..14].copy_from_slice(&r);
-        buffer[14..16].copy_from_slice(&g);
-        buffer[16..18].copy_from_slice(&b);
+        position_max[0] = position_max[0].max(x);
+        position_max[1] = position_max[1].max(y);
+        position_max[2] = position_max[2].max(z);
 
-        bin_content.extend_from_slice(&buffer);
+        position_min[0] = position_min[0].min(x);
+        position_min[1] = position_min[1].min(y);
+        position_min[2] = position_min[2].min(z);
+
+        LittleEndian::write_f64_into(&[x, y, z, r, g, b], &mut buffer);
+        bin_content.write_all(&buffer)?;
     }
 
-    let len_vertices = bin_content.len() - buffer_offset;
+    let byte_length = bin_content.len() - buffer_offset;
 
     gltf_buffer_views.push(BufferView {
         name: Some("vertices".to_string()),
         byte_offset: buffer_offset as u32,
-        byte_length: len_vertices as u32,
-        byte_stride: Some(VERTEX_BYTE_STRIDE as u8),
+        byte_length: byte_length as u32,
+        byte_stride: Some(BYTE_STRIDE as u8),
         target: Some(BufferViewTarget::ArrayBuffer),
         ..Default::default()
     });
@@ -54,8 +61,18 @@ pub fn write_glb<W: Write>(
         buffer_view: Some(gltf_buffer_views.len() as u32 - 1),
         component_type: ComponentType::Float,
         count: points.points.len() as u32,
-        min: Some(points.metadata.bounding_volume.min.to_vec()),
-        max: Some(points.metadata.bounding_volume.max.to_vec()),
+        min: Some(position_min.to_vec()),
+        max: Some(position_max.to_vec()),
+        type_: AccessorType::Vec3,
+        ..Default::default()
+    });
+
+    gltf_accessors.push(Accessor {
+        name: Some("colors".to_string()),
+        buffer_view: Some(gltf_buffer_views.len() as u32 - 1),
+        component_type: ComponentType::Float,
+        byte_offset: 8 * 3,
+        count: points.points.len() as u32,
         type_: AccessorType::Vec3,
         ..Default::default()
     });
