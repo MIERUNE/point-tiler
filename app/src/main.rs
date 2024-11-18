845b18827571fd1ef5669993a1b6a10444f06b82
nokonoko1203 <healtheworld.1203@gmail.com>
2024-11-19 00:26:35 +0900

    Parallelize glb output

--- app/src/main.rs
+++ app/src/main.rs
@@ -24,6 +24,7 @@ use pcd_transformer::{
 use projection_transform::cartesian::geodetic_to_geocentric;
 
 use clap::Parser;
+use rayon::iter::{IntoParallelIterator as _, ParallelIterator as _};
 
 #[derive(Parser, Debug)]
 #[command(
@@ -102,63 +103,65 @@ fn main() {
     let max_zoom = args.max;
     let tiled_pointcloud = pointcloud_to_tiles(&transformed, min_zoom, max_zoom);
 
-    let mut tile_contents: Vec<TileContent> = Default::default();
-    for (tile_coords, pointcloud) in tiled_pointcloud {
-        log::info!("Tile Coords: {:?}", tile_coords);
-        log::info!(
-            "  Number of points: {num_points}",
-            num_points = pointcloud.points.len()
-        );
+    let mut tile_contents: Vec<TileContent> = tiled_pointcloud
+        .into_par_iter()
+        .map(|(tile_coords, pointcloud)| {
+            log::info!("Tile Coords: {:?}", tile_coords);
+            log::info!(
+                "  Number of points: {num_points}",
+                num_points = pointcloud.points.len()
+            );
 
-        let tile_content = make_tile_content(&tile_coords, &pointcloud);
+            let tile_content = make_tile_content(&tile_coords, &pointcloud);
 
-        let mut points = vec![];
-        let ellipsoid = projection_transform::ellipsoid::wgs84();
+            let mut points = vec![];
+            let ellipsoid = projection_transform::ellipsoid::wgs84();
 
-        for point in pointcloud.iter() {
-            let (lng, lat, height) = (point.0, point.1, point.2);
-            let (x, y, z) = geodetic_to_geocentric(&ellipsoid, lng, lat, height);
-            points.push(Point {
-                x,
-                y: z,
-                z: -y,
-                color: point.3.color.clone(),
-                attributes: point.3.attributes.clone(),
-            });
-        }
-        let transformed = PointCloud::new(points, output_epsg);
-        log::info!("  transformed first point: {:?}", transformed.points[0]);
-        log::info!("  offset: {:?}", transformed.metadata.offset);
+            for point in pointcloud.iter() {
+                let (lng, lat, height) = (point.0, point.1, point.2);
+                let (x, y, z) = geodetic_to_geocentric(&ellipsoid, lng, lat, height);
+                points.push(Point {
+                    x,
+                    y: z,
+                    z: -y,
+                    color: point.3.color.clone(),
+                    attributes: point.3.attributes.clone(),
+                });
+            }
+            let transformed = PointCloud::new(points, output_epsg);
+            log::info!("  transformed first point: {:?}", transformed.points[0]);
+            log::info!("  offset: {:?}", transformed.metadata.offset);
 
-        let geometric_error = geometric_error(tile_coords.0, tile_coords.2);
-        log::info!("  Geometric error: {}", geometric_error);
+            let geometric_error = geometric_error(tile_coords.0, tile_coords.2);
+            log::info!("  Geometric error: {}", geometric_error);
 
-        let voxel_size = geometric_error * 0.1;
-        log::info!("  Voxel size: {}", voxel_size);
+            let voxel_size = geometric_error * 0.1;
+            log::info!("  Voxel size: {}", voxel_size);
 
-        let decimetor = VoxelDecimator { voxel_size };
-        let decimated_points = decimetor.decimate(&transformed.points);
-        log::info!(
-            "  Number of decimated points: {num_points}",
-            num_points = decimated_points.len()
-        );
-        let decimated = PointCloud::new(decimated_points, output_epsg);
+            let decimetor = VoxelDecimator { voxel_size };
+            let decimated_points = decimetor.decimate(&transformed.points);
+            log::info!(
+                "  Number of decimated points: {num_points}",
+                num_points = decimated_points.len()
+            );
+            let decimated = PointCloud::new(decimated_points, output_epsg);
 
-        let glb_path = format!(
-            "{}/{}",
-            output_path.to_string_lossy(),
-            tile_content.content_path
-        );
-        log::info!("  write GLB: {:?}", glb_path);
-        std::fs::create_dir_all(std::path::Path::new(&glb_path).parent().unwrap()).unwrap();
+            let glb_path = format!(
+                "{}/{}",
+                output_path.to_string_lossy(),
+                tile_content.content_path
+            );
+            log::info!("  write GLB: {:?}", glb_path);
+            std::fs::create_dir_all(std::path::Path::new(&glb_path).parent().unwrap()).unwrap();
 
-        let glb = generate_quantized_glb(decimated).unwrap();
+            let glb = generate_quantized_glb(decimated).unwrap();
 
-        let writer = std::fs::File::create(glb_path).unwrap();
-        let _ = glb.to_writer_with_alignment(writer, 8);
+            let writer = std::fs::File::create(glb_path).unwrap();
+            let _ = glb.to_writer_with_alignment(writer, 8);
 
-        tile_contents.push(tile_content);
-    }
+            tile_content
+        })
+        .collect();
 
     let mut tree = TileTree::default();
     for content in tile_contents.drain(..) {
