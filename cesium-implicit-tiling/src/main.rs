a875ee77b37ba246c53d4944c28a153e13632c9d
nokonoko1203 <healtheworld.1203@gmail.com>
2024-12-01 11:40:56 +0900

    fix

--- cesium-implicit-tiling/src/main.rs
+++ cesium-implicit-tiling/src/main.rs
@@ -1,22 +1,6 @@
-use cesium_implicit_tiling::{
-    morton_order::{deinterleave_bits, interleave_bits},
-    octree::{BoundingVolume, OctreeNode},
-};
+use cesium_implicit_tiling::octree::{BoundingVolume, OctreeNode};
 
 fn main() {
-    // morton order
-    let x: u32 = 5;
-    let y: u32 = 3;
-    let z: u32 = 7;
-
-    let morton_code = interleave_bits(x, y, z);
-    println!("Morton code: {}", morton_code);
-
-    let (dx, dy, dz) = deinterleave_bits(morton_code);
-    println!("Decoded: x = {}, y = {}, z = {}", dx, dy, dz);
-
-    assert_eq!((x, y, z), (dx, dy, dz));
-
     // subdivide
     let bounding_box = BoundingVolume {
         min: [0.0, 0.0, 0.0],
